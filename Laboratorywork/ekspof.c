736ff27dab75f6902d31dc7e4fa5be7475bf41d9
Kaste245 <adrians.dada@gmail.com>
2022-11-25 01:26:16 +0200

    Class_home

---
+++ Laboratorywork/ekspof.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<math.h>
+
+ int main(){
+
+ double x, y, a, S;
+ int k = 0;
+
+ printf("Eksponentfunkcijas aprēķināšana!\n");
+ scanf("%lf", &x);
+ printf("\nTavs ievadītais argumens x -> %f\n", x);
+
+ y = exp(x);
+ printf("exp(%f) = %f \n\n", x, y);
+
+
+ printf("a0 = %f  S0 = %f\n", a, S);
+
+
+ a = pow(x, k)/(1.);
+ S = a;
+
+ printf("a0 = %f  S0 = %f\n", a, S);
+
+   while(k<500){
+    k++;
+    a = a * x / (k*k);
+    S = S + a;
+   }
+
+ printf("a0 = %f  S0 = %f\n", a, S);
+
+
+
+ printf("       500                              \n");
+ printf("     -------                            \n");
+ printf("     \\        k                        \n");
+ printf(" %.2f \\      x                         \n",x);
+ printf("e    =>    -----                        \n");
+ printf("      /      k!                         \n");
+ printf("     /                                  \n");
+ printf("     -------                            \n");
+ printf("      k=0                               \n");
+ printf("                                        \n");
+ printf("                           x            \n");
+ printf(" Rekurences reizinājums: -----          \n");
+ printf("                          k*k           \n");
+
+ return 0;
+ }
