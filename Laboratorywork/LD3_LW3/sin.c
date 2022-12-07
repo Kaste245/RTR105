f89e1a6f67c341d4ae30904caba7de76be2c9f9e
Kaste245 <adrians.dada@gmail.com>
2022-12-07 19:03:02 +0200

    Class_home

--- Laboratorywork/LD3_LW3/sin.c
+++ Laboratorywork/LD3_LW3/sin.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdlib.h>
 
  int main(){
 
+ FILE *fptr = fopen("derivative.dat", "w");
+
+   if(fptr == NULL)
+    {
+      printf("Error!");
+      exit(1);
+    }
+
  float a=0., b =2*M_PI, x, delta_x=1.e-2, c = 0, d = 0;
 
- printf("\tx\texp(x)\t\texp\'(x)\t\texp\'(x)\t\texp''(x)\texp''(x)\n\n");
- printf("\t\t\t\tanal. forma\tdife. forma\tanal. forma\tdife. forma\n\n");
+ fprintf(fptr, "\tx\texp(x)\t\texp\'(x)\t\texp\'(x)\t\texp''(x)\texp''(x)\n\n");
+ fprintf(fptr, "\t\t\t\tanal. forma\tdife. forma\tanal. forma\tdife. forma\n\n");
 
  x = a;
 
@@ -14,20 +23,20 @@
 
     c = (exp(x + delta_x) - exp(x)) / delta_x;
 
-    d = ((exp(x + delta_x)) - (2*exp(x)) + (exp(x - delta_x))) / (delta_x*delta_x);
+    d = ((exp(x + delta_x)) - (2 * exp(x)) + (exp(x - delta_x))) / (delta_x * delta_x);
 
     x += delta_x;
 
-
     }
-  printf(" %10.2f", x);
-  printf(" %10.2f", exp(x));
-  printf("\t   %10.2f", x);
-  printf("\t     %10.2f", c);
-  printf("\t    %10.2f", x);
-  printf("\t     %10.2f\n", d);
 
+  fprintf(fptr, " %10.2f", x);
+  fprintf(fptr, " %10.2f", exp(x));
+  fprintf(fptr, "\t   %10.2f", x);
+  fprintf(fptr, "\t     %10.2f", c);
+  fprintf(fptr, "\t    %10.2f", x);
+  fprintf(fptr, "\t     %10.2f\n", d);
 
+  fclose(fptr);
 
  return 0;
  }
