5ec6fabb81b5399deb630f7e5673e2c3e150397b
Kaste245 <adrians.dada@gmail.com>
2023-01-11 12:44:57 +0200

    Class_home

--- Laboratorywork/LD2_LW2/diho.c
+++ Laboratorywork/LD2_LW2/diho.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 #include<math.h>
 
@@ -6,21 +7,20 @@
   float a, b, x, c, delta_x, funkca, funkcb, funkcx;
   int k = 0;
 
-
   printf("Ievadi vērtību a\n");
-  scanf("%e", &a);
+  scanf("%f", &a);
 
   printf("Ievadi vērtību b\n");
-  scanf("%e", &b);
+  scanf("%f", &b);
 
   printf("Ievadi vērtību c\n");
-  scanf("%e", &c);
+  scanf("%f", &c);
 
   printf("Ievadi precizitāti\n");
-  scanf("%e", &delta_x);
+  scanf("%f", &delta_x);
 
-  funkca = exp(a);
-  funkcb = exp(b);
+//  funkca = exp(a);
+ // funkcb = exp(b);
 
    /* if(funkca * funkcb > 0){
 
@@ -28,20 +28,22 @@
       printf("Sakņu nav (Paru saknes)\n");
      }*/
 
-    while((b - a) > delta_x){
+
+    while(fabs(exp(x) - c) > delta_x){
+
+    if(exp(x) > c)
+    b = x;
+    else
+    a = x;
 
     k++;
     x = (a + b) / 2;
-
-    if(funkca * exp(x) > 0)
-    a = x;
-    else
-    b = x;
      }
 
+
     printf("Interāciju skaits %d \n", k);
-    printf("Pēc C exp(%7.3f) = %7.3f \n", c, exp(c));
-    printf("Pēc X ar Dihotomijas metodi exp(%3f) = %3f \n", x, exp(x));
+    printf("c = exp(x) --> %f = %.5f \n", c, exp(x));
+    printf("Pēc x ar Dihotomijas metodi exp(%3f) = %3f \n", x, exp(x));
 
  return 0;
  }
--- Laboratorywork/LD2_LW2/gnuscript.gp
+++ Laboratorywork/LD2_LW2/gnuscript.gp
@@ -1,7 +1,10 @@
 
+set term png
+set output 'graphx.png'
+
 set key bmargin center horizontal Right noreverse enhanced autotitle box lt black linewidth 1.000 dashtype solid
 set samples 400, 400
 set grid
-set title "Funkcijas" 
+set title "Funkcijas atvasinājumi" 
 set title  font ",20" textcolor lt -1 norotate
 
