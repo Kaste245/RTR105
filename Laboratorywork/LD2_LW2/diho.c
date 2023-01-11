292ff309daf0577e8a60baa15f5201fe7b8aedcb
Kaste245 <adrians.dada@gmail.com>
2023-01-11 13:45:54 +0200

    Class_

--- Laboratorywork/LD2_LW2/diho.c
+++ Laboratorywork/LD2_LW2/diho.c
@@ -4,7 +4,7 @@
 
  int main(){
 
-  float a, b, x, c, delta_x, funkca, funkcb, funkcx;
+  float a, b, x, c, delta_x, funkca, funkcb;
   int k = 0;
 
   printf("Ievadi vērtību a\n");
@@ -19,31 +19,32 @@
   printf("Ievadi precizitāti\n");
   scanf("%f", &delta_x);
 
-//  funkca = exp(a);
- // funkcb = exp(b);
+  funkca = exp(a);
+  funkcb = exp(b);
 
-   /* if(funkca * funkcb > 0){
+    if(funkca * funkcb > 0){
 
       printf("Intervala [%.2f : %.2f] exp(x) ", a, b);
-      printf("Sakņu nav (Paru saknes)\n");
-     }*/
-
-
-    while(fabs(exp(x) - c) > delta_x){
-
-    if(exp(x) > c)
-    b = x;
-    else
-    a = x;
-
-    k++;
-    x = (a + b) / 2;
+      printf("Sakņu nav (Pāru saknes)\n");
      }
 
+    else{
+
+      while(fabs(exp(x) - c) > delta_x){
+
+       if(exp(x) > c)
+       b = x;
+       else
+       a = x;
+
+       k++;
+       x = (a + b) / 2;
+      }
 
     printf("Interāciju skaits %d \n", k);
     printf("c = exp(x) --> %f = %.5f \n", c, exp(x));
     printf("Pēc x ar Dihotomijas metodi exp(%3f) = %3f \n", x, exp(x));
+    }
 
  return 0;
  }
--- Laboratorywork/LD4_LW4/integral.gp
+++ Laboratorywork/LD4_LW4/integral.gp
@@ -1,10 +1,11 @@
 
-set term png
-set output 'graphx.png'
+#set term png
+#set output 'graphx.png'
 
 set key bmargin center horizontal Right noreverse enhanced autotitle box lt black linewidth 1.000 dashtype solid
 set samples 400, 400
 set grid
-set title "Funkcijas atvasinājumi" 
+set title "Integralis" 
 set title  font ",20" textcolor lt -1 norotate
 
+plot [0:20] [0:100] exp(x) with filledcurves  x1 y1 lc rgb "red"
