67ac394d8fcc57b214004814e38e1da38593b2d0
Kaste245 <adrians.dada@gmail.com>
2023-01-11 19:46:04 +0200

    Class_home

--- Laboratorywork/LD5_LW5/static.c
+++ Laboratorywork/LD5_LW5/static.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include<stdlib.h>
+
  int main(){
 
-  unsigned  char array[127], swap, d, n, c, i, min, max;
-  int total, l, maxvalue = 0, maxcount = 0, j, count = 0, m, len = 0;
+ FILE *fule = fopen("statistic.txt", "w");
 
-  float median;
-
- FILE *fptr = fopen("statistic.dat", "w");
-
-  if(fptr = NULL){
+  if(fule = NULL){
 
   printf("ERROR!");
   exit(1);
   }
 
+  char array[127], swap, d, n, c, i, min, max;
+  int total, l, maxvalue = 0, maxcount = 0, j, count, m, len = 0;
+  float median;
+
   printf("Ievadi kādu daudzumu vēlies sakārtot\n");
   scanf("%hhd", &n);
 
-  printf("Ievadi %hhd simbolus \n", n);
+  printf("Ievadi %d simbolus \n", n);
 
-  for (c = 0; c < n; c++)
-    scanf("%c", &array[c]);
+  for (c = 0; c < n; c++){
+  scanf("%c", &array[c]);
+ //  fgets(array, 5, stdin);
 
+ }
 /*   len = sizeof(array)/sizeof(array[0]);
 
    for(i = 0; i < len; i++){
@@ -32,8 +34,8 @@
               }
             len--;
           }
-      } */
-
+      }
+*/
    //Augoša secība
 
   for (c = 0 ; c < n - 1; c++){
@@ -51,16 +53,18 @@
 
   printf("Sakārtot augošā secībā:\n");
 
-  for (c = 0; c < n; c++)
-    printf("%d \t", array[c]);
-    // fprintf(stdout, "%d \t", array[c]);
-
+  for (c = 0; c < n; c++){
+//    fwrite(array, sizeof(char), sizeof(array),fule);
+    fprintf(stdout, "%d \t", array[c]);
+//     printf("%d \t", array[c]);
+  }
+ fclose(fule);
    //Min, Max
 
     min = max = array[0];
     for(i = 1; i < n; i++)
     {
-         if(min > array[i])
+         if((min > array[i]))
 	  min = array[i];
          if(max < array[i])
           max = array[i];
@@ -87,11 +91,10 @@
 
  printf("Mediānas vērtība = %.2f\n", median);
 
-
   //Moda
 
    for (m = 0; m < n; m++) {
-
+           count = 0;
       for (j = 0; j < n; j++) {
          if (array[j] == array[m])
          ++count;
@@ -105,7 +108,7 @@
 
   printf("Moda = %d\n", maxvalue);
 
-  fclose(fptr);
   return 0;
  }
+
 
